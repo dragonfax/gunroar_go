b3cd43309a31dd56b275c63c71826cf83a071347
Jason Stillwell <dragonfax@gmail.com>
2015-01-11 00:17:40 -0800

    .

--- gr/screen.go
+++ gr/screen.go
@@ -3,317 +3,245 @@
  *
  * Copyright 2005 Kenta Cho. Some rights reserved.
  */
-module abagames.gr.screen;
+package gr
 
-private import std.math;
-private import opengl;
-private import openglu;
-private import abagames.util.rand;
-private import abagames.util.sdl.screen3d;
-private import abagames.util.sdl.luminous;
+import (
+	"math/rand"
+)
 
 /**
  * Initialize an OpenGL and set the caption.
  * Handle a luminous screen and a viewpoint.
  */
-public class Screen: Screen3D {
- private:
-  const char[] CAPTION = "Gunroar";
-  static Rand rand;
-  static float lineWidthBase;
-  LuminousScreen luminousScreen;
-  float _luminosity = 0;
-  int screenShakeCnt;
-  float screenShakeIntense;
 
-  invariant {
-    assert(_luminosity >= 0 && _luminosity <= 1);
-    assert(screenShakeCnt >= 0 && screenShakeCnt < 120);
-    assert(screenShakeIntense >= 0 && screenShakeIntense < 1);
-  }
+const CAPTION = "Gunroar"
 
-  public static this() {
-    rand = new Rand;
-  }
+var lineWidthBase float32
+var brightness float32 = 1
 
-  public static void setRandSeed(long seed) {
-    rand.setSeed(seed);
-  }
-
-  public this() {
-    _luminosity = 0;
-    screenShakeCnt = 0;
-    screenShakeIntense = 0;
-  }
-
-  protected override void init() {
-    setCaption(CAPTION);
-    glLineWidth(1);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE);
-    glEnable(GL_BLEND);
-    glEnable(GL_LINE_SMOOTH);
-    glDisable(GL_TEXTURE_2D);
-    glDisable(GL_COLOR_MATERIAL);
-    glDisable(GL_CULL_FACE);
-    glDisable(GL_DEPTH_TEST);
-    glDisable(GL_LIGHTING);
-    setClearColor(0, 0, 0, 1);
-    if (_luminosity > 0) {
-      luminousScreen = new LuminousScreen;
-      luminousScreen.init(_luminosity, width, height);
-    } else {
-      luminousScreen = null;
-    }
-    screenResized();
-  }
-
-  public override void close() {
-    if (luminousScreen)
-      luminousScreen.close();
-  }
-
-  public bool startRenderToLuminousScreen() {
-    if (!luminousScreen)
-      return false;
-    luminousScreen.startRender();
-    return true;
-  }
-
-  public void endRenderToLuminousScreen() {
-    if (luminousScreen)
-      luminousScreen.endRender();
-  }
-
-  public void drawLuminous() {
-    if (luminousScreen)
-      luminousScreen.draw();
-  }
-
-  public override void resized(int width, int height) {
-    if (luminousScreen)
-      luminousScreen.resized(width, height);
-    super.resized(width, height);
-  }
-
-  public override void screenResized() {
-    super.screenResized();
-    float lw = (cast(float) width / 640 + cast(float) height / 480) / 2;
-    if (lw < 1)
-      lw = 1;
-    else if (lw > 4)
-      lw = 4;
-    lineWidthBase = lw;
-    lineWidth(1);
-  }
-
-  public static void lineWidth(int w) {
-    glLineWidth(cast(int) (lineWidthBase * w));
-  }
-
-  public override void clear() {
-    glClear(GL_COLOR_BUFFER_BIT);
-  }
-
-  public static void viewOrthoFixed() {
-    glMatrixMode(GL_PROJECTION);
-    glPushMatrix();
-    glLoadIdentity();
-    glOrtho(0, 640, 480, 0, -1, 1);
-    glMatrixMode(GL_MODELVIEW);
-    glPushMatrix();
-    glLoadIdentity();
-  }
-
-  public static void viewPerspective() {
-    glMatrixMode(GL_PROJECTION);
-    glPopMatrix();
-    glMatrixMode(GL_MODELVIEW);
-    glPopMatrix();
-  }
-
-  public void setEyepos() {
-    float ex, ey, ez;
-    float lx, ly, lz;
-    ex = ey = 0;
-    ez = 13.0f;
-    lx = ly = lz = 0;
-    if (screenShakeCnt > 0) {
-      float mx = rand.nextSignedFloat(screenShakeIntense * (screenShakeCnt + 4));
-      float my = rand.nextSignedFloat(screenShakeIntense * (screenShakeCnt + 4));
-      ex += mx;
-      ey += my;
-      lx += mx;
-      ly += my;
-    }
-    gluLookAt(ex, ey, ez, lx, ly, lz, 0, 1, 0);
-  }
-
-  public void setScreenShake(int cnt, float its) {
-    screenShakeCnt = cnt;
-    screenShakeIntense = its;
-  }
-
-  public void move() {
-    if (screenShakeCnt > 0)
-      screenShakeCnt--;
-  }
-
-  public float luminosity(float v) {
-    return _luminosity = v;
-  }
-
-  public static void setColorForced(float r, float g, float b, float a = 1) {
-    glColor4f(r, g, b, a);
-  }
+type Screen struct {
+	luminousScreen     LuminousScreen
+	luminosity         float32
+	screenShakeCnt     int
+	screenShakeIntense float32
+	farPlane           float32
+	nearPlane          float32
+	width              int
+	height             int
+	windowMode         bool
 }
-/*
- * $Id: screen3d.d,v 1.1.1.1 2005/06/18 00:46:00 kenta Exp $
- *
- * Copyright 2005 Kenta Cho. Some rights reserved.
- */
-module abagames.util.sdl.screen3d;
 
-private import std.string;
-private import SDL;
-private import opengl;
-private import abagames.util.vector;
-private import abagames.util.sdl.screen;
-private import abagames.util.sdl.sdlexception;
+func (s *Screen) Init() {
+	s.farPlane = 1000
+	s.nearPlane = 0.1
+	s.width = 640
+	s.height = 480
+	s.setCaption(CAPTION)
+	gl.LineWidth(1)
+	gl.BlendFunc(gl.SRC_ALPHA, GL_ONE)
+	gl.Enable(gl.BLEND)
+	gl.Enable(gl.LINE_SMOOTH)
+	gl.Disable(gl.TEXTURE_2D)
+	gl.Disable(gl.COLOR_MATERIAL)
+	gl.Disable(gl.CULL_FACE)
+	gl.Disable(gl.DEPTH_TEST)
+	gl.Disable(gl.LIGHTING)
+	s.setClearColor(0, 0, 0, 1)
+	if s.luminosity > 0 {
+		s.luminousScreen = NewLuminousScreen()
+		s.luminousScreen.Init(s.luminosity, s.width, s.height)
+	} else {
+		s.luminousScreen = nil
+	}
+	s.screenResized()
+}
 
-/**
- * SDL screen handler(3D, OpenGL).
- */
-public class Screen3D: Screen, SizableScreen {
- private:
-  static float _brightness = 1;
-  float _farPlane = 1000;
-  float _nearPlane = 0.1;
-  int _width = 640;
-  int _height = 480;
-  bool _windowMode = false;
+func (s *Screen) Close() {
+	if s.luminousScreen != nil {
+		s.luminousScreen.Close()
+	}
+}
 
-  protected abstract void init();
-  protected abstract void close();
+func (s *Screen) startRenderToLuminousScreen() bool {
+	if s.luminousScreen == nil {
+		return false
+	}
+	s.luminousScreen.startRender()
+	return true
+}
 
-  public void initSDL() {
-    // Initialize SDL.
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-      throw new SDLInitFailedException(
-        "Unable to initialize SDL: " ~ std.string.toString(SDL_GetError()));
-    }
-    // Create an OpenGL screen.
-    Uint32 videoFlags;
-    if (_windowMode) {
-      videoFlags = SDL_OPENGL | SDL_RESIZABLE;
-    } else {
-      videoFlags = SDL_OPENGL | SDL_FULLSCREEN;
-    } 
-    if (SDL_SetVideoMode(_width, _height, 0, videoFlags) == null) {
-      throw new SDLInitFailedException
-        ("Unable to create SDL screen: " ~ std.string.toString(SDL_GetError()));
-    }
-    glViewport(0, 0, width, height);
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    resized(_width, _height);
-    SDL_ShowCursor(SDL_DISABLE);
-    init();
-  }
+func (s *Screen) endRenderToLuminousScreen() {
+	if s.luminousScreen != nil {
+		s.luminousScreen.endRender()
+	}
+}
 
-  // Reset a viewport when the screen is resized.
-  public void screenResized() {
-    glViewport(0, 0, _width, _height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    //gluPerspective(45.0f, cast(GLfloat) width / cast(GLfloat) height, nearPlane, farPlane);
-    glFrustum(-_nearPlane,
-              _nearPlane,
-              -_nearPlane * cast(GLfloat) _height / cast(GLfloat) _width,
-              _nearPlane * cast(GLfloat) _height / cast(GLfloat) _width,
-              0.1f, _farPlane);
-    glMatrixMode(GL_MODELVIEW);
-  }
+func (s *Screen) drawLuminous() {
+	if s.luminousScreen != nil {
+		s.luminousScreen.draw()
+	}
+}
 
-  public void resized(int w, int h) {
-    _width = w;
-    _height = h;
-    screenResized();
-  }
+func (s *Screen) resized(width int, height int) {
+	if s.luminousScreen != nil {
+		s.luminousScreen.resized(width, height)
+	}
 
-  public void closeSDL() {
-    close();
-    SDL_ShowCursor(SDL_ENABLE);
-  }
+	s.width = width
+	s.height = height
+	screenResized()
+}
 
-  public void flip() {
-    handleError();
-    SDL_GL_SwapBuffers();
-  }
+func (s *Screen) screenResized() {
+	gl.Viewport(0, 0, s.width, s.height)
+	gl.MatrixMode(gl.PROJECTION)
+	gl.LoadIdentity()
+	gl.Frustum(-s.nearPlane,
+		s.nearPlane,
+		-s.nearPlane*float32(s.height)/float32(s.width),
+		s.nearPlane*float32(s.height)/float32(s.width),
+		0.1, s.farPlane)
+	gl.MatrixMode(gl.MODELVIEW)
 
-  public void clear() {
-    glClear(GL_COLOR_BUFFER_BIT);
-  }
+	lw := (float32(width)/640 + float32(height)/480) / 2
+	if lw < 1 {
+		lw = 1
+	} else if lw > 4 {
+		lw = 4
+	}
+	lineWidthBase = lw
+	lineWidth(1)
+}
 
-  public void handleError() {
-    GLenum error = glGetError();
-    if (error == GL_NO_ERROR)
-      return;
-    closeSDL();
-    throw new Exception("OpenGL error(" ~ std.string.toString(error) ~ ")");
-  }
+func lineWidth(w int) {
+	gl.LineWidth(int(lineWidthBase * w))
+}
 
-  protected void setCaption(char[] name) {
-    SDL_WM_SetCaption(std.string.toStringz(name), null);
-  }
+func (s *Screen) Clear() {
+	gl.Clear(gl.COLOR_BUFFER_BIT)
+}
 
-  public bool windowMode(bool v) {
-    return _windowMode = v;
-  }
+func viewOrthoFixed() {
+	gl.MatrixMode(gl.PROJECTION)
+	gl.PushMatrix()
+	gl.LoadIdentity()
+	gl.Ortho(0, 640, 480, 0, -1, 1)
+	gl.MatrixMode(gl.MODELVIEW)
+	gl.PushMatrix()
+	gl.LoadIdentity()
+}
 
-  public bool windowMode() {
-    return _windowMode;
-  }
+func viewPerspective() {
+	gl.MatrixMode(gl.PROJECTION)
+	gl.PopMatrix()
+	gl.MatrixMode(gl.MODELVIEW)
+	gl.PopMatrix()
+}
 
-  public int width(int v) {
-    return _width = v;
-  }
+func (s *Screen) setEyepos() {
+	var ex, ey, ez float32
+	var lx, ly, lz float32
+	ez = 13.0
+	if s.screenShakeCnt > 0 {
+		mx = rand.Float32 * (s.screenShakeIntense * (s.screenShakeCnt + 4))
+		my = rand.Float32 * (s.screenShakeIntense * (s.screenShakeCnt + 4))
+		ex += mx
+		ey += my
+		lx += mx
+		ly += my
+	}
+	glu.LookAt(ex, ey, ez, lx, ly, lz, 0, 1, 0)
+}
 
-  public int width() {
-    return _width;
-  }
+func (s *Screen) setScreenShake(cnt int, its float32) {
+	s.screenShakeCnt = cnt
+	s.screenShakeIntense = its
+}
 
-  public int height(int v) {
-    return _height = v;
-  }
+func (s *Screen) move() {
+	if s.screenShakeCnt > 0 {
+		s.screenShakeCnt--
+	}
+}
 
-  public int height() {
-    return _height;
-  }
+func (s *Screen) luminosity(v float32) float32 {
+	s.luminosity = v
+	return s.luminosity
+}
 
-  public static void glVertex(Vector v) {
-    glVertex3f(v.x, v.y, 0);
-  }
+func setColorForced(r float, g float, b float, a float /* = 1 */) {
+	gl.Color4f(r, g, b, a)
+}
 
-  public static void glVertex(Vector3 v) {
-    glVertex3f(v.x, v.y, v.z);
-  }
+func (s *Screen) initSDL() {
+	// Initialize SDL.
+	if sdl.Init(sdl.INIT_VIDEO) < 0 {
+		panic(" SDLInitFailedException( Unable to initialize SDL: " + sdl.GetError())
+	}
+	// Create an OpenGL screen.
+	var videoFlags uint32
+	if s.windowMode {
+		videoFlags = sdl.OPENGL | sdl.RESIZABLE
+	} else {
+		videoFlags = sdl.OPENGL | sdl.FULLSCREEN
+	}
+	if sdl.SetVideoMode(s.width, s.height, 0, videoFlags) == nil {
+		panic("SDLInitFailedException (Unable to create SDL screen: " + sdl.GetError())
+	}
+	gl.Viewport(0, 0, s.width, s.height)
+	gl.ClearColor(0.0, 0.0, 0.0, 0.0)
+	s.resized(s.width, s.height)
+	sdl.ShowCursor(sdl.DISABLE)
+	s.Init()
+}
 
-  public static void glTranslate(Vector v) {
-    glTranslatef(v.x, v.y, 0);
-  }
+func (s *Screen) closeSDL() {
+	s.close()
+	sdl.ShowCursor(sdl.ENABLE)
+}
 
-  public static void glTranslate(Vector3 v) {
-    glTranslatef(v.x, v.y, v.z);
-  }
+func (s *Screen) flip() {
+	s.handleError()
+	gl.SwapBuffers()
+}
 
-  public static void setColor(float r, float g, float b, float a = 1) {
-    glColor4f(r * _brightness, g * _brightness, b * _brightness, a);
-  }
+func (s *Screen) clear() {
+	gl.Clear(gl.COLOR_BUFFER_BIT)
+}
 
-  public static void setClearColor(float r, float g, float b, float a = 1) {
-    glClearColor(r * _brightness, g * _brightness, b * _brightness, a);
-  }
+func (s *Screen) handleError() {
+	error := glGetError()
+	if error == gl.NO_ERROR {
+		return
+	}
+	s.closeSDL()
+	panic("OpenGL error(" + error + ")")
+}
 
-  public static float brightness(float v) {
-    return _brightness = v;
-  }
+func (s *Screen) setCaption(name string) {
+	sdl.WM_SetCaption(name, nil)
+}
+
+func glVertex(v Vector) {
+	gl.Vertex3f(v.X(), v.Y(), 0)
+}
+
+func glVertex3(v Vector3) {
+	gl.Vertex3f(v.X(), v.Y(), v.Z())
+}
+
+func glTranslate(v Vector) {
+	gl.Translatef(v.X(), v.Y(), 0)
+}
+
+func glTranslate3(v Vector3) {
+	gl.Translatef(v.X(), v.Y(), v.Z())
+}
+
+func setColor(r float32, g float32, b float32, a float32 /* = 1 */) {
+	gl.Color4f(r*brightness, g*brightness, b*brightness, a)
+}
+
+func setClearColor(r float32, g float32, b float32, a float32 /*= 1*/) {
+	gl.ClearColor(r*brightness, g*brightness, b*brightness, a)
 }
