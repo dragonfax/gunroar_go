64ed6602701963d3265b7054ea3586de6a5574ab
Jason Stillwell <jstillwell@zendesk.com>
2015-01-13 21:13:49 -0800

    .

--- gr/particle.go
+++ gr/particle.go
@@ -8,16 +8,16 @@ package gtr
 /**
  * Sparks.
  */
-type Spark struct{
+type Spark struct {
 	*LuminousActor
 
-  pos, ppos Vector
-  vel Vector
-  r, g, b float32
-  cnt int
+	pos, ppos Vector
+	vel       Vector
+	r, g, b   float32
+	cnt       int
 }
 
-func NewSpark(p  Vector, vx float32, vy float32 , r float32 , g float32 , b float32 , c int ) *Spark {
+func NewSpark(p Vector, vx float32, vy float32, r float32, g float32, b float32, c int) *Spark {
 	this := &Spark{NewLuminousActor()}
 	this.pos.x = p.x
 	this.ppos.x = p.x
@@ -34,7 +34,7 @@ func NewSpark(p  Vector, vx float32, vy float32 , r float32 , g float32 , b floa
 
 func (this *Spark) move() {
 	cnt--
-	if (cnt <= 0 || vel.dist() < 0.005) {
+	if cnt <= 0 || vel.dist() < 0.005 {
 		exists = false
 		return
 	}
@@ -50,12 +50,12 @@ func (this *Spark) draw() {
 	setScreenColor(r, g, b, 1)
 	ox *= 2
 	oy *= 2
-	gl.Vertex3(pos.x - ox, pos.y - oy, 0)
+	gl.Vertex3(pos.x-ox, pos.y-oy, 0)
 	ox *= 0.5
 	oy *= 0.5
-	setScreenColor(r * 0.5, g * 0.5, b * 0.5, 0)
-	gl.Vertex3(pos.x - oy, pos.y + ox, 0)
-	gl.Vertex3(pos.x + oy, pos.y - ox, 0)
+	setScreenColor(r*0.5, g*0.5, b*0.5, 0)
+	gl.Vertex3(pos.x-oy, pos.y+ox, 0)
+	gl.Vertex3(pos.x+oy, pos.y-ox, 0)
 }
 
 func (this *Spark) drawLuminous() {
@@ -64,19 +64,18 @@ func (this *Spark) drawLuminous() {
 	setScreenColor(r, g, b, 1)
 	ox *= 2
 	oy *= 2
-	gl.Vertex3(pos.x - ox, pos.y - oy, 0)
+	gl.Vertex3(pos.x-ox, pos.y-oy, 0)
 	ox *= 0.5
 	oy *= 0.5
-	setScreenColor(r * 0.5, g * 0.5, b * 0.5, 0)
-	gl.Vertex3(pos.x - oy, pos.y + ox, 0)
-	gl.Vertex3(pos.x + oy, pos.y - ox, 0)
+	setScreenColor(r*0.5, g*0.5, b*0.5, 0)
+	gl.Vertex3(pos.x-oy, pos.y+ox, 0)
+	gl.Vertex3(pos.x+oy, pos.y-ox, 0)
 }
-
 
 /**
  * Smokes.
  */
-type SmokeType int 
+type SmokeType int
 
 const (
 	FIRE SmokeType = iota
@@ -92,16 +91,16 @@ var windVel Vector3 = Vector3{0.04, 0.04, 0.02}
 var wakePos Vector = Vector{}
 
 type Smoke struct {
-	*LuminousActor 
+	*LuminousActor
 
-  field Field
-  pos, vel Vector3
-  smokeType SmokeType
-  cnt, startCnt int
-  size, r, g, b, a float32
+	field            Field
+	pos, vel         Vector3
+	smokeType        SmokeType
+	cnt, startCnt    int
+	size, r, g, b, a float32
 }
 
-func NewSmoke(field Field) *Smoke{
+func NewSmoke(field Field) *Smoke {
 	this = &Smoke{NewLuminousActor()}
 	this.startCnt = 1
 	this.size = 1
@@ -109,17 +108,17 @@ func NewSmoke(field Field) *Smoke{
 	return this
 }
 
-func (this *Smoke) set(p Vector , mx float32 , my float32 , mz float32 , t int , c int /*= 60*/, sz float32 /* = 2 */) {
+func (this *Smoke) set(p Vector, mx float32, my float32, mz float32, t int, c int /*= 60*/, sz float32 /* = 2 */) {
 	set(p.x, p.y, mx, my, mz, t, c, sz)
 }
 
-func (this *Smoke) set(p Vector3 , mx float32 , my float32 , mz float32 , t int , c int /*= 60*/, sz float32 /* = 2*/) {
+func (this *Smoke) set(p Vector3, mx float32, my float32, mz float32, t int, c int /*= 60*/, sz float32 /* = 2*/) {
 	set(p.x, p.y, mx, my, mz, t, c, sz)
 	pos.z = p.z
 }
 
-func (this *Smoke) set(x float32 , y float32 , mx float32 , my float32 , mz float32 , t int , c int /* = 60 */, sz float32 /* = 2 */) {
-	if (!field.checkInOuterField(x, y)) {
+func (this *Smoke) set(x float32, y float32, mx float32, my float32, mz float32, t int, c int /* = 60 */, sz float32 /* = 2 */) {
+	if !field.checkInOuterField(x, y) {
 		return
 	}
 	pos.x = x
@@ -129,9 +128,10 @@ func (this *Smoke) set(x float32 , y float32 , mx float32 , my float32 , mz floa
 	vel.y = my
 	vel.z = mz
 	smokeType = t
-	startCnt = cnt = c
+	startCnt = c
+	cnt = c
 	size = sz
-	switch (smokeType) {
+	switch smokeType {
 	case SmokeType.FIRE:
 		r = rand.nextFloat(0.1) + 0.9
 		g = rand.nextFloat(0.2) + 0.2
@@ -180,22 +180,22 @@ func (this *Smoke) set(x float32 , y float32 , mx float32 , my float32 , mz floa
 
 func (this *Smoke) move() {
 	cnt--
-	if (cnt <= 0 || !field.checkInOuterField(pos.x, pos.y)) {
+	if cnt <= 0 || !field.checkInOuterField(pos.x, pos.y) {
 		exists = false
 		return
 	}
-	if (smokeType != SmokeType.WAKE) {
+	if smokeType != SmokeType.WAKE {
 		vel.x += (windVel.x - vel.x) * 0.01
 		vel.y += (windVel.y - vel.y) * 0.01
 		vel.z += (windVel.z - vel.z) * 0.01
 	}
 	pos += vel
 	pos.y -= field.lastScrollY
-	switch (smokeType) {
+	switch smokeType {
 	case SmokeType.FIRE:
 	case SmokeType.EXPLOSION:
 	case SmokeType.SMOKE:
-		if (cnt < startCnt / 2) {
+		if cnt < startCnt/2 {
 			r *= 0.95
 			g *= 0.95
 			b *= 0.95
@@ -225,33 +225,26 @@ func (this *Smoke) move() {
 		size *= 0.97
 		break
 	}
-	if (size > 5) {
+	if size > 5 {
 		size = 5
 	}
-	if (smokeType == SmokeType.EXPLOSION && pos.z < 0.01) {
+	if smokeType == SmokeType.EXPLOSION && pos.z < 0.01 {
 		bl := field.getBlock(pos.x, pos.y)
-		if (bl >= 1) {
+		if bl >= 1 {
 			vel *= 0.8
 		}
-		if (cnt % 3 == 0 && bl < -1) {
-			sp := sqrt(vel.x * vel.x + vel.y * vel.y)
-			if (sp > 0.3) {
+		if cnt%3 == 0 && bl < -1 {
+			sp := sqrt(vel.x*vel.x + vel.y*vel.y)
+			if sp > 0.3 {
 				d := atan2(vel.x, vel.y)
-				assert(d <>= 0)
-				wakePos.x = pos.x + sin(d + PI / 2) * size * 0.25
-				wakePos.y = pos.y + cos(d + PI / 2) * size * 0.25
-				Wake w = wakes.getInstanceForced()
-				assert(wakePos.x <>= 0)
-				assert(wakePos.y <>= 0)
-				w.set(wakePos, d + PI - 0.2 + rand.nextSignedFloat(0.1), sp * 0.33,
-							20 + rand.nextInt(12), size * (7.0 + rand.nextFloat(3)))
-				wakePos.x = pos.x + sin(d - PI / 2) * size * 0.25
-				wakePos.y = pos.y + cos(d - PI / 2) * size * 0.25
-				w = wakes.getInstanceForced()
-				assert(wakePos.x <>= 0)
-				assert(wakePos.y <>= 0)
-				w.set(wakePos, d + PI + 0.2 + rand.nextSignedFloat(0.1), sp * 0.33,
-							20 + rand.nextInt(12), size * (7.0 + rand.nextFloat(3)))
+				wakePos.x = pos.x + sin(d+PI/2)*size*0.25
+				wakePos.y = pos.y + cos(d+PI/2)*size*0.25
+				NewWake(wakePos, d+PI-0.2+rand.nextSignedFloat(0.1), sp*0.33,
+					20+rand.nextInt(12), size*(7.0+rand.nextFloat(3)))
+				wakePos.x = pos.x + sin(d-PI/2)*size*0.25
+				wakePos.y = pos.y + cos(d-PI/2)*size*0.25
+				NewWake(wakePos, d+PI+0.2+rand.nextSignedFloat(0.1), sp*0.33,
+					20+rand.nextInt(12), size*(7.0+rand.nextFloat(3)))
 			}
 		}
 	}
@@ -260,24 +253,22 @@ func (this *Smoke) move() {
 func (this *Smoke) draw() {
 	quadSize := size / 2
 	setScreenColor(r, g, b, a)
-	gl.Vertex3(pos.x - quadSize, pos.y - quadSize, pos.z)
-	gl.Vertex3(pos.x + quadSize, pos.y - quadSize, pos.z)
-	gl.Vertex3(pos.x + quadSize, pos.y + quadSize, pos.z)
-	gl.Vertex3(pos.x - quadSize, pos.y + quadSize, pos.z)
+	gl.Vertex3(pos.x-quadSize, pos.y-quadSize, pos.z)
+	gl.Vertex3(pos.x+quadSize, pos.y-quadSize, pos.z)
+	gl.Vertex3(pos.x+quadSize, pos.y+quadSize, pos.z)
+	gl.Vertex3(pos.x-quadSize, pos.y+quadSize, pos.z)
 }
 
 func (this *Smoke) drawLuminous() {
-	if (r + g > 0.8 && b < 0.5) {
+	if r+g > 0.8 && b < 0.5 {
 		quadSize := size / 2
 		setScreenColor(r, g, b, a)
-		gl.Vertex3(pos.x - quadSize, pos.y - quadSize, pos.z)
-		gl.Vertex3(pos.x + quadSize, pos.y - quadSize, pos.z)
-		gl.Vertex3(pos.x + quadSize, pos.y + quadSize, pos.z)
-		gl.Vertex3(pos.x - quadSize, pos.y + quadSize, pos.z)
+		gl.Vertex3(pos.x-quadSize, pos.y-quadSize, pos.z)
+		gl.Vertex3(pos.x+quadSize, pos.y-quadSize, pos.z)
+		gl.Vertex3(pos.x+quadSize, pos.y+quadSize, pos.z)
+		gl.Vertex3(pos.x-quadSize, pos.y+quadSize, pos.z)
 	}
 }
-}
-
 
 /**
  * Fragments of destroyed enemies.
@@ -285,13 +276,13 @@ func (this *Smoke) drawLuminous() {
 var fragmentDisplayList *DisplayList
 
 type Fragment struct {
-  field Field
-  pos, vel Vector3
-  size, d2, md2 float32
+	field         Field
+	pos, vel      Vector3
+	size, d2, md2 float32
 }
 
 func InitFragments() {
-	fragmentDisplayList = new DisplayList(1)
+	fragmentDisplayList = NewDisplayList(1)
 	fragmentDisplayList.beginNewList()
 	setScreenColor(0.7, 0.5, 0.5, 0.5)
 	gl.Begin(gl.TRIANgl.E_FAN)
@@ -321,8 +312,8 @@ func NewFragment(field Field) {
 	return this
 }
 
-func (this *Fragment) set( p Vector , mx float32 , my float32 , mz float32 , sz float32 /* = 1*/) {
-	if (!field.checkInOuterField(p.x, p.y)) {
+func (this *Fragment) set(p Vector, mx float32, my float32, mz float32, sz float32 /* = 1*/) {
+	if !field.checkInOuterField(p.x, p.y) {
 		return
 	}
 	pos.x = p.x
@@ -332,7 +323,7 @@ func (this *Fragment) set( p Vector , mx float32 , my float32 , mz float32 , sz 
 	vel.y = my
 	vel.z = mz
 	size = sz
-	if (size > 5) {
+	if size > 5 {
 		size = 5
 	}
 	d2 = rand.nextFloat(360)
@@ -341,7 +332,7 @@ func (this *Fragment) set( p Vector , mx float32 , my float32 , mz float32 , sz 
 }
 
 func (this *Fragment) move() {
-	if (!field.checkInOuterField(pos.x, pos.y)) {
+	if !field.checkInOuterField(pos.x, pos.y) {
 		exists = false
 		return
 	}
@@ -349,12 +340,11 @@ func (this *Fragment) move() {
 	vel.y *= 0.96
 	vel.z += (-0.04 - vel.z) * 0.01
 	pos += vel
-	if (pos.z < 0) {
-		Smoke s = smokes.getInstanceForced()
-		if (field.getBlock(pos.x, pos.y) < 0) {
-			s.set(pos.x, pos.y, 0, 0, 0, Smoke.SmokeType.WAKE, 60, size * 0.66)
+	if pos.z < 0 {
+		if field.getBlock(pos.x, pos.y) < 0 {
+			NewSmoke(pos.x, pos.y, 0, 0, 0, Smoke.SmokeType.WAKE, 60, size*0.66)
 		} else {
-			s.set(pos.x, pos.y, 0, 0, 0, Smoke.SmokeType.SAND, 60, size * 0.75)
+			NewSmoke(pos.x, pos.y, 0, 0, 0, Smoke.SmokeType.SAND, 60, size*0.75)
 		}
 		exists = false
 		return
@@ -372,7 +362,6 @@ func (this *Fragment) draw() {
 	gl.PopMatrix()
 }
 
-
 /**
  * Luminous fragments.
  */
@@ -381,15 +370,15 @@ var sparkFragmentdisplayList *DisplayList
 type SparkFragment struct {
 	*LuminousActor
 
-  field Field
-  pos, vel Vector3
-  size, d2, md2 float32
-  cnt int
-  hasSmoke bool
+	field         Field
+	pos, vel      Vector3
+	size, d2, md2 float32
+	cnt           int
+	hasSmoke      bool
 }
 
 func InitSparkFragments() {
-	sparkFragmentDisplayList = new DisplayList(1)
+	sparkFragmentDisplayList = NewDisplayList(1)
 	sparkFragmentDisplayList.beginNewList()
 	gl.Begin(gl.TRIANGLE_FAN)
 	gl.Vertex2(-0.25, -0.25)
@@ -411,8 +400,8 @@ func NewSparkFragment(field Field) *SparkFragment {
 	return this
 }
 
-func (this *SparkFragment) set(Vector p, float32 mx, float32 my, float32 mz, float32 sz = 1) {
-	if (!field.checkInOuterField(p.x, p.y)) {
+func (this *SparkFragment) set(p Vector, mx float32, my float32, mz float32, sz float32 /*= 1*/) {
+	if !field.checkInOuterField(p.x, p.y) {
 		return
 	}
 	pos.x = p.x
@@ -422,12 +411,12 @@ func (this *SparkFragment) set(Vector p, float32 mx, float32 my, float32 mz, flo
 	vel.y = my
 	vel.z = mz
 	size = sz
-	if (size > 5) {
+	if size > 5 {
 		size = 5
 	}
 	d2 = rand.nextFloat(360)
 	md2 = rand.nextSignedFloat(15)
-	if (rand.nextInt(4) == 0) {
+	if rand.nextInt(4) == 0 {
 		hasSmoke = true
 	} else {
 		hasSmoke = false
@@ -437,7 +426,7 @@ func (this *SparkFragment) set(Vector p, float32 mx, float32 my, float32 mz, flo
 }
 
 func (this *SparkFragment) move() {
-	if (!field.checkInOuterField(pos.x, pos.y)) {
+	if !field.checkInOuterField(pos.x, pos.y) {
 		exists = false
 		return
 	}
@@ -445,12 +434,11 @@ func (this *SparkFragment) move() {
 	vel.y *= 0.99
 	vel.z += (-0.08 - vel.z) * 0.01
 	pos += vel
-	if (pos.z < 0) {
-		Smoke s = smokes.getInstanceForced()
-		if (field.getBlock(pos.x, pos.y) < 0) {
-			s.set(pos.x, pos.y, 0, 0, 0, Smoke.SmokeType.WAKE, 60, size * 0.66)
+	if pos.z < 0 {
+		if field.getBlock(pos.x, pos.y) < 0 {
+			NewSmoke(pos.x, pos.y, 0, 0, 0, Smoke.SmokeType.WAKE, 60, size*0.66)
 		} else {
-			s.set(pos.x, pos.y, 0, 0, 0, Smoke.SmokeType.SAND, 60, size * 0.75)
+			NewSmoke(pos.x, pos.y, 0, 0, 0, Smoke.SmokeType.SAND, 60, size*0.75)
 		}
 		exists = false
 		return
@@ -458,10 +446,9 @@ func (this *SparkFragment) move() {
 	pos.y -= field.lastScrollY
 	d2 += md2
 	cnt++
-	if (hasSmoke && cnt % 5 == 0) {
-		Smoke s = smokes.getInstance()
-		if (s) {
-			s.set(pos, 0, 0, 0, Smoke.SmokeType.SMOKE, 90 + rand.nextInt(60), size * 0.5)
+	if hasSmoke && cnt%5 == 0 {
+		if s {
+			NewSmoke(pos, 0, 0, 0, Smoke.SmokeType.SMOKE, 90+rand.nextInt(60), size*0.5)
 		}
 	}
 }
@@ -485,18 +472,16 @@ func (this *SparkFragment) drawLuminous() {
 	displayList.call(0)
 	gl.PopMatrix()
 }
-}
-
 
 /**
  * Wakes of ships and smokes.
  */
 type Wake struct {
-  field Field
-  pos, vel Vector
-  deg, speed, size float32
-  cnt int
-  revShape bool
+	field            Field
+	pos, vel         Vector
+	deg, speed, size float32
+	cnt              int
+	revShape         bool
 }
 
 func NewWake(field Field) *Wake {
@@ -506,8 +491,8 @@ func NewWake(field Field) *Wake {
 	return this
 }
 
-func (this *Wake) set(p Vector , deg float32 , speed float32 speed, c int /*= 60*/, sz float32 /*= 1*/ , rs bool /* = false */) {
-	if (!field.checkInOuterField(p.x, p.y)) {
+func (this *Wake) set(p Vector, deg float32, speed float32, c int /*= 60*/, sz float32 /*= 1*/, rs bool /* = false */) {
+	if !field.checkInOuterField(p.x, p.y) {
 		return
 	}
 	pos.x = p.x
@@ -524,7 +509,7 @@ func (this *Wake) set(p Vector , deg float32 , speed float32 speed, c int /*= 60
 
 func (this *Wake) move() {
 	cnt--
-	if (cnt <= 0 || vel.dist() < 0.005 || !field.checkInOuterField(pos.x, pos.y)) {
+	if cnt <= 0 || vel.dist() < 0.005 || !field.checkInOuterField(pos.x, pos.y) {
 		exists = false
 		return
 	}
@@ -540,15 +525,14 @@ func (this *Wake) draw() {
 	setScreenColor(0.33, 0.33, 1)
 	ox *= size
 	oy *= size
-	if (revShape) {
-		gl.Vertex3(pos.x + ox, pos.y + oy, 0)
+	if revShape {
+		gl.Vertex3(pos.x+ox, pos.y+oy, 0)
 	} else {
-		gl.Vertex3(pos.x - ox, pos.y - oy, 0)
+		gl.Vertex3(pos.x-ox, pos.y-oy, 0)
 	}
 	ox *= 0.2
 	oy *= 0.2
 	setScreenColor(0.2, 0.2, 0.6, 0.5)
-	gl.Vertex3(pos.x - oy, pos.y + ox, 0)
-	gl.Vertex3(pos.x + oy, pos.y - ox, 0)
+	gl.Vertex3(pos.x-oy, pos.y+ox, 0)
+	gl.Vertex3(pos.x+oy, pos.y-ox, 0)
 }
-
