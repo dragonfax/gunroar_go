6d5f9ce52584598d56afd29d6ca06ceed7762cb7
Jason Stillwell <jstillwell@zendesk.com>
2015-01-13 19:39:37 -0800

    .

--- gr/stagemanager.go
+++ gr/stagemanager.go
@@ -3,338 +3,285 @@
  *
  * Copyright 2005 Kenta Cho. Some rights reserved.
  */
-module abagames.gr.stagemanager;
-
-private import std.string;
-private import std.math;
-private import abagames.util.vector;
-private import abagames.util.rand;
-private import abagames.util.sdl.shape;
-private import abagames.gr.enemy;
-private import abagames.gr.bullet;
-private import abagames.gr.ship;
-private import abagames.gr.field;
-private import abagames.gr.letter;
-private import abagames.gr.particle;
-private import abagames.gr.soundmanager;
+package gr
 
 /**
  * Manage an enemys' appearance, a rank(difficulty) and a field.
  */
-public class StageManager {
- private:
-  static const float RANK_INC_BASE = 0.0018f;
-  static const int BLOCK_DENSITY_MIN = 0;
-  static const int BLOCK_DENSITY_MAX = 3;
-  Field field;
-  EnemyPool enemies;
-  Ship ship;
-  BulletPool bullets;
-  SparkPool sparks;
-  SmokePool smokes;
-  FragmentPool fragments;
-  WakePool wakes;
-  Rand rand;
-  float rank, baseRank, addRank, rankVel, rankInc;
-  EnemyAppearance[] enemyApp;
-  int _blockDensity;
-  int batteryNum;
-  PlatformEnemySpec platformEnemySpec;
-  bool _bossMode;
-  int bossAppCnt;
-  int bossAppTime, bossAppTimeBase;
-  int bgmStartCnt;
+const RANK_INC_BASE = 0.0018
+const BLOCK_DENSITY_MIN = 0
+const BLOCK_DENSITY_MAX = 3
 
-  invariant {
-    assert(rank >= 1);
-    assert(baseRank >= 1);
-    assert(addRank >= 0);
-    assert(rankVel <>= 0);
-    assert(rankInc <>= 0);
-    assert(_blockDensity >= BLOCK_DENSITY_MIN && _blockDensity <= BLOCK_DENSITY_MAX);
-    assert(batteryNum >= 0 && batteryNum < 50);
-  }
-
-  public this(Field field, EnemyPool enemies, Ship ship, BulletPool bullets,
-              SparkPool sparks, SmokePool smokes, FragmentPool fragments, WakePool wakes) {
-    this.field = field;
-    this.enemies = enemies;
-    this.ship = ship;
-    this.bullets = bullets;
-    this.sparks = sparks;
-    this.smokes = smokes;
-    this.fragments = fragments;
-    this.wakes = wakes;
-    rand = new Rand;
-    enemyApp = new EnemyAppearance[3];
-    foreach (inout EnemyAppearance ea; enemyApp)
-      ea = new EnemyAppearance;
-    PlatformEnemySpec platformEnemySpec =
-      new PlatformEnemySpec(field, ship, sparks, smokes, fragments, wakes);
-    rank = baseRank = 1;
-    addRank = rankVel = rankInc = 0;
-    _blockDensity = 2;
-  }
-
-  public void setRandSeed(long seed) {
-    rand.setSeed(seed);
-  }
-
-  public void start(float rankIncRatio) {
-    rank = baseRank = 1;
-    addRank = rankVel = 0;
-    rankInc = RANK_INC_BASE * rankIncRatio;
-    _blockDensity = rand.nextInt(BLOCK_DENSITY_MAX - BLOCK_DENSITY_MIN + 1) + BLOCK_DENSITY_MIN;
-    _bossMode = false;
-    bossAppTimeBase = 60 * 1000;
-    resetBossMode();
-    gotoNextBlockArea();
-    bgmStartCnt = -1;
-  }
-
-  public void startBossMode() {
-    _bossMode = true;
-    bossAppCnt = 2;
-    SoundManager.fadeBgm();
-    bgmStartCnt = 120;
-    rankVel = 0;
-  }
-
-  public void resetBossMode() {
-    if (_bossMode) {
-      _bossMode = false;
-      SoundManager.fadeBgm();
-      bgmStartCnt = 120;
-      bossAppTimeBase += 30 * 1000;
-    }
-    bossAppTime = bossAppTimeBase;
-  }
-
-  public void move() {
-    bgmStartCnt--;
-    if (bgmStartCnt == 0) {
-      if (_bossMode)
-        SoundManager.playBgm("gr0.ogg");
-      else
-        SoundManager.nextBgm();
-    }
-    if (_bossMode) {
-      addRank *= 0.999f;
-      if (!enemies.hasBoss && bossAppCnt <= 0)
-        resetBossMode();
-    } else {
-      float rv = field.lastScrollY / ship.scrollSpeedBase - 2;
-      bossAppTime -= 17;
-      if (bossAppTime <= 0) {
-        bossAppTime = 0;
-        startBossMode();
-      }
-      if (rv > 0) {
-        rankVel += rv * rv * 0.0004f * baseRank;
-      } else {
-        rankVel += rv * baseRank;
-        if (rankVel < 0)
-          rankVel = 0;
-      }
-      addRank += rankInc * (rankVel + 1);
-      addRank *= 0.999f;
-      baseRank += rankInc + addRank * 0.0001f;
-    }
-    rank = baseRank + addRank;
-    foreach (EnemyAppearance ea; enemyApp)
-      ea.move(enemies, field);
-  }
-
-  public void shipDestroyed() {
-    rankVel = 0;
-    if (!_bossMode)
-      addRank = 0;
-    else
-      addRank /= 2;
-  }
-
-  public void gotoNextBlockArea() {
-    if (_bossMode) {
-      bossAppCnt--;
-      if (bossAppCnt == 0) {
-        ShipEnemySpec ses = new ShipEnemySpec(field, ship, sparks, smokes, fragments, wakes);
-        ses.setParam(rank, ShipEnemySpec.ShipClass.BOSS, rand);
-        Enemy en = enemies.getInstance();
-        if (en) {
-          if ((cast(HasAppearType) ses).setFirstState(en.state, EnemyState.AppearanceType.CENTER))
-            en.set(ses);
-        } else {
-          resetBossMode();
-        }
-      }
-      foreach (EnemyAppearance ea; enemyApp)
-        ea.unset();
-      return;
-    }
-    bool noSmallShip;
-    if (_blockDensity < BLOCK_DENSITY_MAX && rand.nextInt(2) == 0)
-      noSmallShip = true;
-    else
-      noSmallShip = false;
-    _blockDensity += rand.nextSignedInt(1);
-    if (_blockDensity < BLOCK_DENSITY_MIN)
-      _blockDensity = BLOCK_DENSITY_MIN;
-    else if (_blockDensity > BLOCK_DENSITY_MAX)
-      _blockDensity = BLOCK_DENSITY_MAX;
-    batteryNum = cast(int) ((_blockDensity + rand.nextSignedFloat(1)) * 0.75f);
-    float tr = rank;
-    int largeShipNum = cast(int) ((2 - _blockDensity + rand.nextSignedFloat(1)) * 0.5f);
-    if (noSmallShip)
-      largeShipNum *= 1.5f;
-    else
-      largeShipNum *= 0.5f;
-    int appType = rand.nextInt(2);
-    if (largeShipNum > 0) {
-      float lr = tr * (0.25f + rand.nextFloat(0.15f));
-      if (noSmallShip)
-        lr *= 1.5f;
-      tr -= lr;
-      ShipEnemySpec ses = new ShipEnemySpec(field, ship, sparks, smokes, fragments, wakes);
-      ses.setParam(lr / largeShipNum, ShipEnemySpec.ShipClass.LARGE, rand);
-      enemyApp[0].set(ses, largeShipNum, appType, rand);
-    } else {
-      enemyApp[0].unset();
-    }
-    if (batteryNum > 0) {
-      platformEnemySpec = new PlatformEnemySpec(field, ship, sparks, smokes, fragments, wakes);
-      float pr = tr * (0.3f + rand.nextFloat(0.1f));
-      platformEnemySpec.setParam(pr / batteryNum, rand);
-    }
-    appType = (appType + 1) % 2;
-    int middleShipNum = cast(int) ((4 - _blockDensity + rand.nextSignedFloat(1)) * 0.66f);
-    if (noSmallShip)
-      middleShipNum *= 2;
-    if (middleShipNum > 0) {
-      float mr;
-      if (noSmallShip)
-        mr = tr;
-      else
-        mr = tr * (0.33f + rand.nextFloat(0.33f));
-      tr -= mr;
-      ShipEnemySpec ses = new ShipEnemySpec(field, ship, sparks, smokes, fragments, wakes);
-      ses.setParam(mr / middleShipNum, ShipEnemySpec.ShipClass.MIDDLE, rand);
-      enemyApp[1].set(ses, middleShipNum, appType, rand);
-    } else {
-      enemyApp[1].unset();
-    }
-    if (!noSmallShip) {
-      appType = EnemyState.AppearanceType.TOP;
-      int smallShipNum =
-        cast(int) (sqrt(3 + tr) * (1 + rand.nextSignedFloat(0.5f)) * 2) + 1;
-      if (smallShipNum > 256)
-        smallShipNum = 256;
-      SmallShipEnemySpec sses = new SmallShipEnemySpec(field, ship, sparks, smokes, fragments, wakes);
-      sses.setParam(tr / smallShipNum, rand);
-      enemyApp[2].set(sses, smallShipNum, appType, rand);
-    } else {
-      enemyApp[2].unset();
-    }
-  }
-
-  public void addBatteries(PlatformPos[] platformPos, int platformPosNum) {
-    int ppn = platformPosNum;
-    int bn = batteryNum;
-    for (int i = 0; i < 100; i++) {
-      if (ppn <= 0 || bn <= 0)
-        break;
-      int ppi = rand.nextInt(platformPosNum);
-      for (int j = 0; j < platformPosNum; j++) {
-        if (!platformPos[ppi].used)
-          break;
-        ppi++;
-        if (ppi >= platformPosNum)
-          ppi = 0;
-      }
-      if (platformPos[ppi].used)
-        break;
-      Enemy en = enemies.getInstance();
-      if (!en)
-        break;
-      platformPos[ppi].used = true;
-      ppn--;
-      Vector p = field.convertToScreenPos
-        (cast(int) platformPos[ppi].pos.x, cast(int) platformPos[ppi].pos.y);
-      if (!platformEnemySpec.setFirstState(en.state, p.x, p.y, platformPos[ppi].deg))
-        continue;
-      for (int i = 0; i < platformPosNum; i++) {
-        if (fabs(platformPos[ppi].pos.x - platformPos[i].pos.x) <= 1 &&
-            fabs(platformPos[ppi].pos.y - platformPos[i].pos.y) <= 1 &&
-            !platformPos[i].used) {
-          platformPos[i].used = true;
-          ppn--;
-        }
-      }
-      en.set(platformEnemySpec);
-      bn--;
-    }
-  }
-
-  public int blockDensity() {
-    return _blockDensity;
-  }
-
-  public void draw() {
-    Letter.drawNum(cast(int) (rank * 1000), 620, 10, 10, 0, 0, 33, 3);
-    Letter.drawTime(bossAppTime, 120, 20, 7);
-  }
-
-  public float rankMultiplier() {
-    return rank;
-  }
-
-  public bool bossMode() {
-    return _bossMode;
-  }
+type StageManager struct {
+  field Field
+  ship Ship
+  rank, baseRank, addRank, rankVel, rankInc float32
+  enemyApp [3]*EnemyAppearance
+  blockDensity int
+  batteryNum int
+  platformEnemySpec PlatformEnemySpec
+  bossMode bool
+  bossAppCnt int
+  bossAppTime, bossAppTimeBase int
+  bgmStartCnt int
 }
 
-public class EnemyAppearance {
- private:
-  EnemySpec spec;
-  float nextAppDist, nextAppDistInterval;
-  int appType;
+func NewStateManager(field Field, ship Ship) {
+	this.field = field
+	this.ship = ship
+	for i,_ := range this.enemyApp {
+		this.enemyApp[i] = NewEnemyAppearance()
+	}
+	this.platformEnemySpec = NewPlatformEnemySpec(field, ship)
+	this.rank =  1
+	this.baseRank = 1
+	this.blockDensity = 2
+}
 
-  invariant {
-    assert(nextAppDist <>= 0);
-    assert(nextAppDistInterval > 0);
-    assert(appType >= 0);
-  }
+func (this *StateManager)  start(rankIncRatio float32) {
+	this.rank = 1
+	this.baseRank = 1
+	this.addRank = 0
+	this.rankVel = 0
+	this.rankInc = RANK_INC_BASE * this.rankIncRatio
+	this.blockDensity = rand.Int(BLOCK_DENSITY_MAX - BLOCK_DENSITY_MIN + 1) + BLOCK_DENSITY_MIN
+	this.bossMode = false
+	this.bossAppTimeBase = 60 * 1000
+	this.resetBossMode()
+	this.gotoNextBlockArea()
+	this.bgmStartCnt = -1
+}
 
-  public this() {
-    nextAppDist = 0;
-    nextAppDistInterval = 1;
-  }
+func (this *StateManager)  startBossMode() {
+	this.bossMode = true
+	this.bossAppCnt = 2
+	fadeBgm()
+	this.bgmStartCnt = 120
+	this.rankVel = 0
+}
 
-  public void set(EnemySpec s, int num, int appType, Rand rand) {
-    spec = s;
-    nextAppDistInterval = (cast(float) Field.NEXT_BLOCK_AREA_SIZE) / num;
-    nextAppDist = rand.nextFloat(nextAppDistInterval);
-    this.appType = appType;
-  }
+func (this *StateManager)  resetBossMode() {
+	if (this.bossMode) {
+		this.bossMode = false
+		fadeBgm()
+		this.bgmStartCnt = 120
+		this.bossAppTimeBase += 30 * 1000
+	}
+	this.bossAppTime = this.bossAppTimeBase
+}
 
-  public void unset() {
-    spec = null;
-  }
+func (this *StageManager) move() {
+	this.bgmStartCnt--
+	if (this.bgmStartCnt == 0) {
+		if (this.bossMode) {
+			playBgm("gr0.ogg")
+		}
+		else {
+			nextBgm()
+		}
+	}
+	if (_bossMode) {
+		this.addRank *= 0.999
+		if (!enemies.hasBoss() && this.bossAppCnt <= 0) {
+			this.resetBossMode()
+		}
+	} else {
+		rv := this.field.lastScrollY / this.ship.scrollSpeedBase - 2
+		this.bossAppTime -= 17
+		if (this.bossAppTime <= 0) {
+			this.bossAppTime = 0
+			this.startBossMode()
+		}
+		if (rv > 0) {
+			this.rankVel += rv * rv * 0.0004 * this.baseRank
+		} else {
+			this.rankVel += rv * this.baseRank
+			if (this.rankVel < 0) {
+				this.rankVel = 0
+			}
+		}
+		this.addRank += this.rankInc * (this.rankVel + 1)
+		this.addRank *= 0.999
+		this.baseRank += this.rankInc + this.addRank * 0.0001
+	}
+	this.rank = this.baseRank + this.addRank
+	for _,ea : range this.enemyApp {
+		ea.move(this.field)
+	}
+}
 
-  public void move(EnemyPool enemies, Field field) {
-    if (!spec)
-      return;
-    nextAppDist -= field.lastScrollY;
-    if (nextAppDist <= 0) {
-      nextAppDist += nextAppDistInterval;
-      appear(enemies);
-    }
-  }
+func (this *StageManager) shipDestroyed() {
+	this.rankVel = 0
+	if (!this.bossMode) {
+		this.addRank = 0
+	}
+	else {
+		this.addRank /= 2
+	}
+}
 
-  private void appear(EnemyPool enemies) {
-    Enemy en = enemies.getInstance();
-    if (en) {
-      if ((cast(HasAppearType) spec).setFirstState(en.state, appType))
-        en.set(spec);
-    }
-  }
+func (this *StageManager)  gotoNextBlockArea() {
+	if (this.bossMode) {
+		this.bossAppCnt--
+		if (this.bossAppCnt == 0) {
+			ses := NewShipEnemySpec(this.field, this.ship)
+			ses.setParam(rank, ShipEnemySpec.ShipClass.BOSS, rand)
+			en = NewEnemy()
+			if ((cast(HasAppearType) ses).setFirstState(en.state, EnemyState.AppearanceType.CENTER)) {
+				en.set(ses)
+			}
+		}
+		for _,ea := range this.enemyApp) {
+			ea.remove()
+		}
+		return
+	}
+	noSmallShip bool
+	if (this.blockDensity < BLOCK_DENSITY_MAX && rand.Int(2) == 0) {
+		noSmallShip = true
+	} else {
+		noSmallShip = false
+	}
+	this.blockDensity += rand.SignedInt(1)
+	if (this.blockDensity < BLOCK_DENSITY_MIN) {
+		this.blockDensity = BLOCK_DENSITY_MIN
+	} else if (this.blockDensity > BLOCK_DENSITY_MAX) {
+		this.blockDensity = BLOCK_DENSITY_MAX
+	}
+	this.batteryNum = (this.blockDensity + rand.SignedFloat(1)) * 0.75
+	tr := this.rank
+	largeShipNum := (2 - this.blockDensity + rand.SignedFloat(1)) * 0.5
+	if (noSmallShip) {
+		largeShipNum *= 1.5
+	} else {
+		largeShipNum *= 0.5
+	}
+	appType := rand.Int(2)
+	if (largeShipNum > 0) {
+		lr := tr * (0.25 + rand.nextFloat(0.15))
+		if (noSmallShip) {
+			lr *= 1.5
+		}
+		tr -= lr
+		ses := NewShipEnemySpec(field, ship)
+		ses.setParam(lr / largeShipNum, ShipEnemySpec.ShipClass.LARGE, rand)
+		this.enemyApp[0].set(ses, largeShipNum, appType, rand)
+	} else {
+		this.enemyApp[0].remove()
+	}
+	if (batteryNum > 0) {
+		this.platformEnemySpec = NewPlatformEnemySpec(field, ship, sparks, smokes, fragments, wakes)
+		pr := tr * (0.3f + rand.nextFloat(0.1))
+		this.platformEnemySpec.setParam(pr / batteryNum, rand)
+	}
+	appType = (appType + 1) % 2
+	middleShipNum := ((4 - _blockDensity + rand.nextSignedFloat(1)) * 0.66
+	if (noSmallShip) {
+		middleShipNum *= 2
+	}
+	if (middleShipNum > 0) {
+		var mr float32
+		if (noSmallShip) {
+			mr = tr
+		} else {
+			mr = tr * (0.33f + rand.nextFloat(0.33f))
+		}
+		tr -= mr
+		ses := NewShipEnemySpec(field, ship)
+		ses.setParam(mr / middleShipNum, ShipEnemySpec.ShipClass.MIDDLE, rand)
+		this.enemyApp[1].set(ses, middleShipNum, appType, rand)
+	} else {
+		this.enemyApp[1].remove()
+	}
+	if (!noSmallShip) {
+		appType = EnemyState.AppearanceType.TOP
+		smallShipNum :=  (sqrt(3 + tr) * (1 + rand.nextSignedFloat(0.5f)) * 2) + 1
+		if (smallShipNum > 256) {
+			smallShipNum = 256
+		}
+		SmallShipEnemySpec sses = new SmallShipEnemySpec(field, ship, sparks, smokes, fragments, wakes)
+		sses.setParam(tr / smallShipNum, rand)
+		enemyApp[2].set(sses, smallShipNum, appType, rand)
+	} else {
+		enemyApp[2].unset()
+	}
+}
+
+func (this *StageManager)  addBatteries(platformPos []PlatformPos, platformPosNum int) {
+	ppn := platformPosNum
+	bn := batteryNum
+	for i := 0; i < 100; i++ {
+		if (ppn <= 0 || bn <= 0) {
+			break
+		}
+		ppi := rand.Int(platformPosNum)
+		for j := 0; j < platformPosNum; j++ {
+			if (!platformPos[ppi].used) {
+				break
+			}
+			ppi++
+			if (ppi >= platformPosNum) {
+				ppi = 0
+			}
+		}
+		if (platformPos[ppi].used) {
+			break
+		}
+		en := NewEnemy()
+		platformPos[ppi].used = true
+		ppn--
+		p := this.field.convertToScreenPos(platformPos[ppi].pos.x, platformPos[ppi].pos.y)
+		if (!platformEnemySpec.setFirstState(en.state, p.x, p.y, platformPos[ppi].deg)) {
+			continue
+		}
+		for i := 0; i < platformPosNum; i++ {
+			if (fabs32(platformPos[ppi].pos.x - platformPos[i].pos.x) <= 1 &&
+					fabs32(platformPos[ppi].pos.y - platformPos[i].pos.y) <= 1 &&
+					!platformPos[i].used) {
+				platformPos[i].used = true
+				ppn--
+			}
+		}
+		en.set(platformEnemySpec)
+		bn--
+	}
+}
+
+func (this *StageManager) draw() {
+	drawNum(this.rank * 1000, 620, 10, 10, 0, 0, 33, 3)
+	drawTime(this.bossAppTime, 120, 20, 7)
+}
+
+
+type EnemyAppearance struct {
+	spec EnemySpec
+	nextAppDist, nextAppDistInterval float32
+	appType int
+}
+
+func NewEnemyAppearance( s EnemySpec, num int, appType int) *EnemyAppearance {
+	this := new(EnemyAppearance)
+	this.nextAppDistInterval = 1
+	this.spec = s
+	this.nextAppDistInterval = NEXT_BLOCK_AREA_SIZE / num
+	this.nextAppDist = rand.SignedFloat(nextAppDistInterval)
+	this.appType = appType
+	return this
+}
+
+func (this *EnemyAppearance)  move(field Field) {
+	if (spec == nil ) {
+		return
+	}
+	this.nextAppDist -= this.field.lastScrollY
+	if (this.nextAppDist <= 0) {
+		this.nextAppDist += this.nextAppDistInterval
+		this.appear()
+	}
+}
+
+func (this *EnemyAppearance) appear() {
+	if this.spec.setFirstState(en.state, this.appType) {
+		NewEnemy(this.spec)
+	}
 }
