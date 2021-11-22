dffb7ccda37e0af1b68ad9641e36114c98eb3d8a
Jason Stillwell <dragonfax@gmail.com>
2021-11-22 04:18:22 -0800

    just started

---
+++ gr/math.go
@@ -0,0 +1,19 @@
+package util
+
+import "math"
+
+func normalizeDeg(d float64) float64 {
+	if d < -math.Pi {
+		d = math.Pi*2 - math.Mod(-d, (math.Pi*2))
+	}
+	d = math.Mod((d+math.Pi), (math.Pi*2)) - math.Pi
+	return d
+}
+
+func normalizeDeg360(d float64) float64 {
+	if d < -180 {
+		d = 360 - math.Mod(-d, 360)
+	}
+	d = math.Mod((d+180), 360) - 180
+	return d
+}
