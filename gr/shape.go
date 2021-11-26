0e94ec39ef79f3829a9812675a56c8d77c9970c3
Jason Stillwell <dragonfax@gmail.com>
2021-11-26 01:07:46 -0800

    .

--- gr/shape.go
+++ gr/shape.go
@@ -43,27 +43,27 @@ public class EnemyShape: ResizableDrawable {
 
   public static void init() {
     shapes ~= new BaseShape
-      (1, 0.5, 0.1f, BaseShape.ShapeType.SHIP, 0.9f, 0.7f, 0.5f);
-    shapes ~= new BaseShape
-      (1, 0.5f, 0.1f, BaseShape.ShapeType.SHIP_DAMAGED, 0.5f, 0.5f, 0.9f);
+      (1, 0.5, 0.1, BaseShape.ShapeType.SHIP, 0.9, 0.7, 0.5);
+    /hapes ~= new BaseShape
+      (1, 0.5, 0.1, BaseShape.ShapeType.SHIP_DAMAGED, 0.5, 0.5, 0.9);
     shapes ~= new CollidableBaseShape
-      (0.66f, 0, 0, BaseShape.ShapeType.BRIDGE, 1, 0.2f, 0.3f);
+      (0.66, 0, 0, BaseShape.ShapeType.BRIDGE, 1, 0.2, 0.3);
     shapes ~= new BaseShape
-      (1, 0.7f, 0.33f, BaseShape.ShapeType.SHIP, MIDDLE_COLOR_R, MIDDLE_COLOR_G, MIDDLE_COLOR_B);
+      (1, 0.7, 0.33, BaseShape.ShapeType.SHIP, MIDDLE_COLOR_R, MIDDLE_COLOR_G, MIDDLE_COLOR_B);
     shapes ~= new BaseShape
-      (1, 0.7f, 0.33f, BaseShape.ShapeType.SHIP_DAMAGED, 0.5f, 0.5f, 0.9f);
+      (1, 0.7, 0.33, BaseShape.ShapeType.SHIP_DAMAGED, 0.5, 0.5, 0.9);
     shapes ~= new BaseShape
-      (1, 0.7f, 0.33f, BaseShape.ShapeType.SHIP_DESTROYED, 0, 0, 0);
+      (1, 0.7, 0.33, BaseShape.ShapeType.SHIP_DESTROYED, 0, 0, 0);
     shapes ~= new CollidableBaseShape
-      (0.66f, 0, 0, BaseShape.ShapeType.BRIDGE, 1, 0.2f, 0.3f);
+      (0.66, 0, 0, BaseShape.ShapeType.BRIDGE, 1, 0.2, 0.3);
     shapes ~= new BaseShape
-      (1, 0, 0, BaseShape.ShapeType.PLATFORM, 1, 0.6f, 0.7f);
+      (1, 0, 0, BaseShape.ShapeType.PLATFORM, 1, 0.6, 0.7);
     shapes ~= new BaseShape
-      (1, 0, 0, BaseShape.ShapeType.PLATFORM_DAMAGED, 0.5f, 0.5f, 0.9f);
+      (1, 0, 0, BaseShape.ShapeType.PLATFORM_DAMAGED, 0.5, 0.5, 0.9);
     shapes ~= new BaseShape
-      (1, 0, 0, BaseShape.ShapeType.PLATFORM_DESTROYED, 1, 0.6f, 0.7f);
+      (1, 0, 0, BaseShape.ShapeType.PLATFORM_DESTROYED, 1, 0.6, 0.7);
     shapes ~= new CollidableBaseShape
-      (0.5f, 0, 0, BaseShape.ShapeType.BRIDGE, 1, 0.2f, 0.3f);
+      (0.5, 0, 0, BaseShape.ShapeType.BRIDGE, 1, 0.2, 0.3);
   }
 
   public static void close() {
@@ -112,26 +112,26 @@ public class BulletShape: ResizableDrawable {
 public class NormalBulletShape: DrawableShape {
   public override void createDisplayList() {
     glDisable(GL_BLEND);
-    Screen.setColor(1, 1, 0.3f);
+    Screen.setColor(1, 1, 0.3);
     glBegin(GL_LINE_STRIP);
-    glVertex3f(0.2f, -0.25f, 0.2f);
-    glVertex3f(0, 0.33f, 0);
-    glVertex3f(-0.2f, -0.25f, -0.2f);
+    glVertex3f(0.2, -0.25, 0.2);
+    glVertex3f(0, 0.33, 0);
+    glVertex3f(-0.2, -0.25, -0.2);
     glEnd();
     glBegin(GL_LINE_STRIP);
-    glVertex3f(-0.2f, -0.25f, 0.2f);
-    glVertex3f(0, 0.33f, 0);
-    glVertex3f(0.2f, -0.25f, -0.2f);
+    glVertex3f(-0.2, -0.25, 0.2);
+    glVertex3f(0, 0.33, 0);
+    glVertex3f(0.2, -0.25, -0.2);
     glEnd();
     glEnable(GL_BLEND);
-    Screen.setColor(0.5f, 0.2f, 0.1f);
+    Screen.setColor(0.5, 0.2, 0.1);
     glBegin(GL_TRIANGLE_FAN);
-    glVertex3f(0, 0.33f, 0);
-    glVertex3f(0.2f, -0.25f, 0.2f);
-    glVertex3f(-0.2f, -0.25f, 0.2f);
-    glVertex3f(-0.2f, -0.25f, -0.2f);
-    glVertex3f(0.2f, -0.25f, -0.2f);
-    glVertex3f(0.2f, -0.25f, 0.2f);
+    glVertex3f(0, 0.33, 0);
+    glVertex3f(0.2, -0.25, 0.2);
+    glVertex3f(-0.2, -0.25, 0.2);
+    glVertex3f(-0.2, -0.25, -0.2);
+    glVertex3f(0.2, -0.25, -0.2);
+    glVertex3f(0.2, -0.25, 0.2);
     glEnd();
   }
 }
@@ -139,26 +139,26 @@ public class NormalBulletShape: DrawableShape {
 public class SmallBulletShape: DrawableShape {
   public override void createDisplayList() {
     glDisable(GL_BLEND);
-    Screen.setColor(0.6f, 0.9f, 0.3f);
+    Screen.setColor(0.6, 0.9, 0.3);
     glBegin(GL_LINE_STRIP);
-    glVertex3f(0.25f, -0.25f, 0.25f);
-    glVertex3f(0, 0.33f, 0);
-    glVertex3f(-0.25f, -0.25f, -0.25f);
+    glVertex3f(0.25, -0.25, 0.25);
+    glVertex3f(0, 0.33, 0);
+    glVertex3f(-0.25, -0.25, -0.25);
     glEnd();
     glBegin(GL_LINE_STRIP);
-    glVertex3f(-0.25f, -0.25f, 0.25f);
-    glVertex3f(0, 0.33f, 0);
-    glVertex3f(0.25f, -0.25f, -0.25f);
+    glVertex3f(-0.25, -0.25, 0.25);
+    glVertex3f(0, 0.33, 0);
+    glVertex3f(0.25, -0.25, -0.25);
     glEnd();
     glEnable(GL_BLEND);
-    Screen.setColor(0.2f, 0.4f, 0.1f);
+    Screen.setColor(0.2, 0.4, 0.1);
     glBegin(GL_TRIANGLE_FAN);
-    glVertex3f(0, 0.33f, 0);
-    glVertex3f(0.25f, -0.25f, 0.25f);
-    glVertex3f(-0.25f, -0.25f, 0.25f);
-    glVertex3f(-0.25f, -0.25f, -0.25f);
-    glVertex3f(0.25f, -0.25f, -0.25f);
-    glVertex3f(0.25f, -0.25f, 0.25f);
+    glVertex3f(0, 0.33, 0);
+    glVertex3f(0.25, -0.25, 0.25);
+    glVertex3f(-0.25, -0.25, 0.25);
+    glVertex3f(-0.25, -0.25, -0.25);
+    glVertex3f(0.25, -0.25, -0.25);
+    glVertex3f(0.25, -0.25, 0.25);
     glEnd();
   }
 }
@@ -166,26 +166,26 @@ public class SmallBulletShape: DrawableShape {
 public class MovingTurretBulletShape: DrawableShape {
   public override void createDisplayList() {
     glDisable(GL_BLEND);
-    Screen.setColor(0.7f, 0.5f, 0.9f);
+    Screen.setColor(0.7, 0.5, 0.9);
     glBegin(GL_LINE_STRIP);
-    glVertex3f(0.25f, -0.25f, 0.25f);
-    glVertex3f(0, 0.33f, 0);
-    glVertex3f(-0.25f, -0.25f, -0.25f);
+    glVertex3f(0.25, -0.25, 0.25);
+    glVertex3f(0, 0.33, 0);
+    glVertex3f(-0.25, -0.25, -0.25);
     glEnd();
     glBegin(GL_LINE_STRIP);
-    glVertex3f(-0.25f, -0.25f, 0.25f);
-    glVertex3f(0, 0.33f, 0);
-    glVertex3f(0.25f, -0.25f, -0.25f);
+    glVertex3f(-0.25, -0.25, 0.25);
+    glVertex3f(0, 0.33, 0);
+    glVertex3f(0.25, -0.25, -0.25);
     glEnd();
     glEnable(GL_BLEND);
-    Screen.setColor(0.2f, 0.2f, 0.3f);
+    Screen.setColor(0.2, 0.2, 0.3);
     glBegin(GL_TRIANGLE_FAN);
-    glVertex3f(0, 0.33f, 0);
-    glVertex3f(0.25f, -0.25f, 0.25f);
-    glVertex3f(-0.25f, -0.25f, 0.25f);
-    glVertex3f(-0.25f, -0.25f, -0.25f);
-    glVertex3f(0.25f, -0.25f, -0.25f);
-    glVertex3f(0.25f, -0.25f, 0.25f);
+    glVertex3f(0, 0.33, 0);
+    glVertex3f(0.25, -0.25, 0.25);
+    glVertex3f(-0.25, -0.25, 0.25);
+    glVertex3f(-0.25, -0.25, -0.25);
+    glVertex3f(0.25, -0.25, -0.25);
+    glVertex3f(0.25, -0.25, 0.25);
     glEnd();
   }
 }
@@ -197,22 +197,22 @@ public class DestructiveBulletShape: DrawableShape, Collidable {
 
   public override void createDisplayList() {
     glDisable(GL_BLEND);
-    Screen.setColor(0.9f, 0.9f, 0.6f);
+    Screen.setColor(0.9, 0.9, 0.6);
     glBegin(GL_LINE_LOOP);
-    glVertex3f(0.2f, 0, 0);
-    glVertex3f(0, 0.4f, 0);
-    glVertex3f(-0.2f, 0, 0);
-    glVertex3f(0, -0.4f, 0);
+    glVertex3f(0.2, 0, 0);
+    glVertex3f(0, 0.4, 0);
+    glVertex3f(-0.2, 0, 0);
+    glVertex3f(0, -0.4, 0);
     glEnd();
     glEnable(GL_BLEND);
-    Screen.setColor(0.7f, 0.5f, 0.4f);
+    Screen.setColor(0.7, 0.5, 0.4);
     glBegin(GL_TRIANGLE_FAN);
-    glVertex3f(0.2f, 0, 0);
-    glVertex3f(0, 0.4f, 0);
-    glVertex3f(-0.2f, 0, 0);
-    glVertex3f(0, -0.4f, 0);
+    glVertex3f(0.2, 0, 0);
+    glVertex3f(0, 0.4, 0);
+    glVertex3f(-0.2, 0, 0);
+    glVertex3f(0, -0.4, 0);
     glEnd();
-    _collision = new Vector(0.4f, 0.4f);
+    _collision = new Vector(0.4, 0.4);
   }
 
   public Vector collision() {
@@ -222,19 +222,19 @@ public class DestructiveBulletShape: DrawableShape, Collidable {
 
 public class CrystalShape: DrawableShape {
   public override void createDisplayList() {
-    Screen.setColor(0.6f, 1, 0.7f);
+    Screen.setColor(0.6, 1, 0.7);
     glBegin(GL_LINE_LOOP);
-    glVertex3f(-0.2f, 0.2f, 0);
-    glVertex3f(0.2f, 0.2f, 0);
-    glVertex3f(0.2f, -0.2f, 0);
-    glVertex3f(-0.2f, -0.2f, 0);
+    glVertex3f(-0.2, 0.2, 0);
+    glVertex3f(0.2, 0.2, 0);
+    glVertex3f(0.2, -0.2, 0);
+    glVertex3f(-0.2, -0.2, 0);
     glEnd();
   }
 }
 
 public class ShieldShape: DrawableShape {
   public override void createDisplayList() {
-    Screen.setColor(0.5f, 0.5f, 0.7f);
+    Screen.setColor(0.5, 0.5, 0.7);
     glBegin(GL_LINE_LOOP);
     float d = 0;
     for (int i = 0; i < 8; i++) {
@@ -246,7 +246,7 @@ public class ShieldShape: DrawableShape {
     Screen.setColor(0, 0, 0);
     glVertex3f(0, 0, 0);
     d = 0;
-    Screen.setColor(0.3f, 0.3f, 0.5f);
+    Screen.setColor(0.3, 0.3, 0.5);
     for (int i = 0; i < 9; i++) {
       glVertex3f(sin(d), cos(d), 0);
       d += PI / 4;
