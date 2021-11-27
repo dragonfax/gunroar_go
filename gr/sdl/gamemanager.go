f5fc69fe62208240eb3a78e01e776d0ecc77d33d
Jason Stillwell <dragonfax@gmail.com>
2021-11-26 21:41:00 -0800

    .

---
+++ gr/sdl/gamemanager.go
@@ -0,0 +1,44 @@
+package sdl
+
+
+/**
+ * Manage the lifecycle of the game.
+ */
+
+type GameManager interface {
+  init();
+  start();
+  close();
+  move();
+  draw();
+
+  setMainLoop(*MainLoop)
+  setUIs(Screen, Input)
+  setPrefManager(PrefManager)
+}
+
+type GameManagerBase struct {
+  MainLoop mainLoop;
+  Screen abstScreen;
+  Input input;
+  PrefManager abstPrefManager;
+}
+
+func NewGameManagerBaseInternal() GameManagerBase{
+  return GameManagerBase{}
+}
+
+  func (this *GameManagerBase) setMainLoop(mainLoop MainLoop) {
+    this.mainLoop = mainLoop;
+  }
+
+  func (this *GameManagerBase)public void setUIs(screen Screen, input Input) {
+    this.abstScreen = screen;
+    this.input = input;
+  }
+
+  func (this *GameManagerBase)public void setPrefManager(prefManager PrefManager) {
+    this.abstPrefManager = prefManager;
+  }
+
+}
